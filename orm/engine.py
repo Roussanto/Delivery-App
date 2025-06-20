699266a32000fb1575c291dcbb9fc4aca04fa42e
Roussanto <roussanto171@gmail.com>
2025-06-20 20:14:19 +0300

    Created sqlalchemy model

--- orm/engine.py
+++ orm/engine.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(
-    "mysql+pymysql://root:Roussanto171!@localhost:3306",
+    "mysql+pymysql://root:Roussanto171!@localhost:3306/income",
     pool_pre_ping=True
 )
 
--- orm/model.py
+++ orm/model.py
@@ -1,28 +1,198 @@
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String, Float, Numeric, Enum
+from sqlalchemy import Column, Integer, String, Float, Numeric, Date, Time, Enum, ForeignKey
 
 Base = declarative_base()
 
 
-class Address(Base):
-    __tablename__ = "addresses"
+class Coffee(Base):
+    __tablename__ = "coffees"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(40))
-    latitude = Column(Float(precision=53))
-    longitude = Column(Float(precision=53))
+    type = Column(Enum("espresso", "espresso machiatto", "espresso americano", "cappuccino latte", "nes", "frappe"), nullable=False),
+    size = Column(Enum("single", "double", "quadruple"), nullable=False)
+    variety = Column(Enum("80% arabica + 20% robust", "100% arabica", "decaffeine"), nullable=False)
+    sugar = Column(Enum("little", "medium", "medium-to-sweet", "sweet", "very sweet"))
+    sugar_type = Column(Enum("white", "brown", "saccharin", "stevia", "honey"))
+    milk = Column(Enum("fresh", "evapore", "almond", "coconut", "oat"))
+    extra = Column(Enum("chocolate syrup", "strawberry syrup", "caramel syrup", "hazelnut syrup", "vanilla syrup", "cinnamon powder", "chocolate powder"))
+    cost = Column(Numeric(precision=4, scale=2), nullable=False)
+
+
+class FreddoFlat(Base):
+    __tablename__ = "freddos_flats"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    type = Column(Enum("freddo espresso", "espresso cappuccino", "espresso cappuccino latte", "flat white cold", "flat white hot"), nullable=False),
+    size = Column(Enum("regular", "XL"), nullable=False)
+    variety = Column(Enum("80% arabica + 20% robust", "100% arabica", "decaffeine"), nullable=False)
+    sugar = Column(Enum("little", "medium", "medium-to-sweet", "sweet", "very sweet"))
+    sugar_type = Column(Enum("white", "brown", "saccharin", "stevia", "honey"))
+    milk = Column(Enum("fresh", "evapore", "almond", "coconut", "oat"))
+    extra = Column(Enum("chocolate syrup", "strawberry syrup", "caramel syrup", "hazelnut syrup", "vanilla syrup", "cinnamon powder", "chocolate powder"))
+    cost = Column(Numeric(precision=4, scale=2), nullable=False)
+
+
+class Filter(Base):
+    __tablename__ = "filters"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    type = Column(Enum("filter", "irish coffee", "ellinikos"), nullable=False),
+    size = Column(Enum("single", "double"), nullable=False)
+    sugar = Column(Enum("little", "medium", "medium-to-sweet", "sweet", "very sweet"))
+    sugar_type = Column(Enum("white", "brown", "saccharin", "stevia", "honey"))
+    milk = Column(Enum("fresh", "evapore", "almond", "coconut", "oat"))
+    extra = Column(Enum("chocolate syrup", "strawberry syrup", "caramel syrup", "hazelnut syrup", "vanilla syrup", "cinnamon powder", "chocolate powder"))
+    cost = Column(Numeric(precision=4, scale=2), nullable=False)
+
+
+class Chocolate(Base):
+    __tablename__ = "chocolates"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    type = Column(Enum("chocolate", "chocolate viennois", "chocolate white", "chocolate bitter", "chocolate orange", "chocolate hazelnut", "chocolate ruby", "chocolate salty caramel"), nullable=False)
+    temperature = Column(Enum("hot", "cold"), nullable=False)
+    extra = Column(Enum("chocolate syrup", "strawberry syrup", "caramel syrup", "hazelnut syrup", "vanilla syrup"))
+    cost = Column(Numeric(precision=4, scale=2), nullable=False)
+
+
+class WeirdChocolate(Base):
+    __tablename__ = "weird_chocolates"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    type = Column(Enum("freddoccino", "mochaccino"), nullable=False)
+    temperature = Column(Enum("hot", "cold"), nullable=False)
+    extra = Column(Enum("chocolate syrup", "strawberry syrup", "caramel syrup", "hazelnut syrup", "vanilla syrup"))
+    cost = Column(Numeric(precision=4, scale=2), nullable=False)
 
 
 class Beverage(Base):
     __tablename__ = "beverages"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    type = Column(String(40))
-    cost = Column(Numeric(precision=4, scale=2))
+    type = Column(String(40), nullable=False)
+    cost = Column(Numeric(precision=4, scale=2), nullable=False)
+
+
+class Tee(Base):
+    __tablename__ = "tees"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    type = Column(Enum("tee"), nullable=False),
+    sugar = Column(Enum("little", "medium", "medium-to-sweet", "sweet", "very sweet"))
+    sugar_type = Column(Enum("white", "brown", "saccharin", "stevia", "honey"))
+    milk = Column(Enum("fresh", "evapore", "almond", "coconut", "oat"))
+    extra = Column(Enum("chocolate syrup", "strawberry syrup", "caramel syrup", "hazelnut syrup", "vanilla syrup", "cinnamon powder", "chocolate powder"))
+    cost = Column(Numeric(precision=4, scale=2), nullable=False)
+
 
 class Chamomile(Base):
     __tablename__ = "chamomiles"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
+    type = Column(Enum("chamomile"), nullable=False),
+    sugar = Column(Enum("little", "medium", "medium-to-sweet", "sweet", "very sweet"))
+    sugar_type = Column(Enum("white", "brown", "saccharin", "stevia", "honey"))
+    milk = Column(Enum("fresh", "evapore", "almond", "coconut", "oat"))
+    extra = Column(Enum("chocolate syrup", "strawberry syrup", "caramel syrup", "hazelnut syrup", "vanilla syrup", "cinnamon powder", "chocolate powder"))
+    cost = Column(Numeric(precision=4, scale=2), nullable=False)
+
+
+class Smoothie(Base):
+    __tablename__ = "smoothies"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    type = Column(Enum("smoothie: ergati", "smoothie: kiklothimikou", "smoothie: popai", "smoothie: athliti", "smoothie: mogli", "smoothie: irakli"), nullable=False)
+    milk = Column(Enum("light", "fat", "almond", "coconut"))
+    cost = Column(Numeric(precision=4, scale=2), nullable=False)
+
+
+class Food(Base):
+    __tablename__ = "foods"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
     type = Column(String(40))
-    cost = Column(Numeric(precision=4, scale=2))
+    cost = Column(Numeric(precision=4, scale=2), nullable=False)
+
+
+class Offer(Base):
+    __tablename__ = "offers"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    description = Column(String(40), nullable=False)
+    cost = Column(Numeric(precision=4, scale=2), nullable=False)
+
+
+class Address(Base):
+    __tablename__ = "addresses"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(40), nullable=False)
+    latitude = Column(Float(precision=53))
+    longitude = Column(Float(precision=53))
+
+
+class Customer(Base):
+    __tablename__ = "customers"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(40), nullable=False)
+    floor = Column(Integer, nullable=False)
+
+    address_id = Column(Integer, ForeignKey("customers.id"), nullable=False)
+
+
+class Workday(Base):
+    __tablename__ = "workdays"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    date = Column(Date, unique=True, nullable=False)
+    hours = Column(Numeric(precision=4, scale=2), nullable=False)
+    payment = Column(Numeric(precision=4, scale=2), nullable=False)
+
+
+class Order(Base):
+    __tablename_ = "orders"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    order_time = Column(Time, nullable=False)
+    delivery_time = Column(Time)
+    tips = Column(Numeric(precision=4, scale=2), nullable=False)
+    tips_method = Column(Enum("Cash", "Card"))
+    source = Column(Enum("Efood", "Wolt", "Box", "Phone"), nullable=True)
+    payment_method = Column(Enum("Cash", "Card"), nullable=True)
+
+    customer_id = Column(Integer, ForeignKey("customers.id"), nullable=False)
+    workday_id = Column(Integer, ForeignKey("workdays.id"), nullable=False)
+
+
+class Item(Base):
+    __tablename_ = "items"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    product_id = Column(Integer, nullable=False)
+    product_type = Column(Enum("coffee", "freddo or flat", "filter", "chocolate", "weird chocolate", "beverage", "tee", "chamomile", "smoothie", "food"), nullable=False)
+
+    order_id = Column(Integer, ForeignKey("orders.id"), nullable=False)
+    offer_id = Column(Integer, ForeignKey("offers.id"), nullable=False)
+
+
+# Read view
+class VOrdersSummary(Base):
+    __tablename_ = "items"
+
+    order_id = Column(Integer)
+    order_time = Column(Date)
+    tips = Column(Numeric(precision=4, scale=2))
+    tips_method = Column(String(40))
+    source = Column(String(40))
+    payment_method = Column(String(40))
+    name = Column(String(40))
+    latitude = Column(Float(precision=53))
+    longitude = Column(Float(precision=53))
+
+
+
+
+
+
+
